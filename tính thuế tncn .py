8fbffdec23382639e4fb25cc9901366194014534
Thaotesla <thaonthvac@gmail.com>
2022-07-21 22:28:53 +0700

    Create tính thuế tncn .py

---
+++ tính thuế tncn .py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+a=11000000
+b=4400000
+songuoiphuthuoc=2
+tongthunhap=25000000
+thunhapchiuthue= tongthunhap-11-songuoiphuthuoc*4.4
+print(thunhapchiuthue)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
